1234899a78c5322369279865015e23083fb11b27
Aathisankar-A <aathisankarayyappan@gmail.com>
2026-01-10 00:08:32 +0530

    updated middle panel and menu with event to simulate the output based on the otion clicked

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import { useView } from "../../Context/ViewContext/ViewContext";
 
 import './Menu.css';
 
 export default function Menu() {
+  const { setCurrentView } = useView();
+
   return (
     <div className="menu">
       <h2> 
         <em> Menu </em>
       </h2>
-      <h4> For You </h4>
-      <h4> Podcasts </h4>
-      <h4> Top 50 </h4>
+      <h4 onClick={() => setCurrentView("for_you")}>For You</h4>
+      <h4 onClick={() => setCurrentView("top_50")}>Top 50</h4>
+      <h4 onClick={() => setCurrentView("podcast")}>Podcasts</h4>
     </div>
   )
 }
--- src/components/MiddlePanel/MiddlePanel.jsx
+++ src/components/MiddlePanel/MiddlePanel.jsx
@@ -5,16 +5,27 @@ import './MiddlePanel.css';
 
 import RunningCard from '../RunningCard/RunningCard';
 import PlayerControls from '../PlayerControls/PlayerControls';
+
+import { useView } from "../../Context/ViewContext/ViewContext";
+
+import ForYou from "../Menu/ForYou/ForYou";
+import Podcast from "../Menu/Podcast/Podcast";
+import Top50 from "../Menu/Top50/Top50";
+
 const SuggestionArea = lazy(() => import("../SuggestionArea/SuggestionArea"));
 
 
 export default function MiddlePanel() {
+  const { currentView } = useView();
+
   return(
     <div className="middle-panel">
       <RunningCard/>
       <PlayerControls/>
       <Suspense fallback={<div className="loader">Loading songs...</div>}>
-        <SuggestionArea />
+        {currentView === "for_you" && <ForYou />}
+        {currentView === "podcast" && <Podcast />}
+        {currentView === "top_50" && <Top50 />}
       </Suspense>
     </div>
   )
