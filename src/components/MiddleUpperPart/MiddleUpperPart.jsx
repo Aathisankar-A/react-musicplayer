37cc2b0d4ebeb99fe126be1a93e0c472cc827e0a
Aathisankar-A <aathisankarayyappan@gmail.com>
2026-01-19 12:11:38 +0530

    added searchContext for search feature

--- src/components/MiddleUpperPart/MiddleUpperPart.jsx
+++ src/components/MiddleUpperPart/MiddleUpperPart.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+import { useContext } from "react";
+import { SearchContext } from "../../Context/SearchContext/SearchContext.jsx";
 
 import './MiddleUpperPart.css';
 
-export default function LeftUpperPart() {
+export default function MiddleUpperPart() {
+  const { setQuery } = useContext(SearchContext);
+
   return(
     <div className="middle-upper-part">
       <input
         className="search-bar"
         type="text"
         placeholder="Search songs, artists..."
+        onChange={(e) => setQuery(e.target.value)}
       />
     </div>
   )
